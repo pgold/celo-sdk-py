761a0bd481f856df9a347be69f6ea3846d138c16
pgold <pgold@users.noreply.github.com>
2022-07-02 11:18:14 +0100

    WIP: Add Exchange{BRL,EUR}Wrapper.

---
+++ celo_sdk/contracts/ExchangeBRLWrapper.py
@@ -0,0 +1,11 @@
+import sys
+
+from celo_sdk.contracts.base_wrapper import BaseWrapper
+from celo_sdk.contracts.ExchangeWrapper import Exchange
+from celo_sdk.registry import Registry
+
+from web3 import Web3
+
+
+class ExchangeBRL(Exchange):
+    pass
